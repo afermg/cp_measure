e08fe063f1b9354118ebba4e6850c8bbdab6b0e0
Alan Munoz <afer.mg@gmail.com>
2024-08-18 01:19:42 -0400

    add utils.py

---
+++ src/cp_measure/measurements/utils.py
@@ -0,0 +1,9 @@
+
+import numpy
+
+def get_test_pixels_mask():
+    pixels = numpy.random.randint(100,size=64**2).reshape((64,64))
+    mask = numpy.ones_like(pixels,dtype=bool)
+    return pixels, mask
+
+pixels, masks = get_test_pixels_mask()
