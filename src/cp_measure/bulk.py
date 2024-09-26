d7633f531dc91a2961870b04178a83e310b8b367
Alán F. Muñoz <afer.mg@gmail.com>
2024-09-26 12:45:47 -0400

    change(bulk): move import inside convenience functions

--- src/cp_measure/bulk.py
+++ src/cp_measure/bulk.py
@@ -5,16 +5,16 @@ match the original names. This may change in the future.
 
 from typing import Callable
 
-from cp_measure.minimal import (
-    measuregranularity,
-    measureobjectintensity,
-    measureobjectintensitydistribution,
-    measureobjectsizeshape,
-    measuretexture,
-)
-
-
 def get_all_measurements() -> dict[str, Callable]:
+    
+    from cp_measure.minimal import (
+        measuregranularity,
+        measureobjectintensity,
+        measureobjectintensitydistribution,
+        measureobjectsizeshape,
+        measuretexture,
+    )
+
     return {
         "radial_distribution": measureobjectintensitydistribution.get_radial_distribution,
         "radial_zernikes": measureobjectintensitydistribution.get_radial_zernikes,
@@ -24,4 +24,26 @@ def get_all_measurements() -> dict[str, Callable]:
         "ferret": measureobjectsizeshape.get_ferret,
         "granularity": measuregranularity.get_granularity,
         "texture": measuretexture.get_texture,
+    }
+
+def get_fast_measurements() -> dict[str, Callable]:
+    from cp_measure.fast import (
+        measuregranularity,
+        measureobjectintensity,
+        measureobjectintensitydistribution,
+        measureobjectsizeshape,
+        measuretexture,
+    )
+
+
+    return {
+        "radial_distribution": measureobjectintensitydistribution.get_radial_distribution,
+        "radial_zernikes": measureobjectintensitydistribution.get_radial_zernikes,
+        "intensity": measureobjectintensity.get_intensity,
+        "sizeshape": measureobjectsizeshape.get_sizeshape,
+        "zernike": measureobjectsizeshape.get_zernike,
+        "ferret": measureobjectsizeshape.get_ferret,
+        "texture": measuretexture.get_texture,        
+        "granularity": measuregranularity.get_granularity,
+
     }
