7fa408dd0eb3773bb07986822bcded20de2c2cca
Alán F. Muñoz <afer.mg@gmail.com>
2024-09-27 14:37:25 -0400

    clean: rm minimal

--- src/cp_measure/bulk.py
+++ src/cp_measure/bulk.py
@@ -5,27 +5,6 @@ match the original names. This may change in the future.
 
 from typing import Callable
 
-def get_all_measurements() -> dict[str, Callable]:
-    
-    from cp_measure.minimal import (
-        measuregranularity,
-        measureobjectintensity,
-        measureobjectintensitydistribution,
-        measureobjectsizeshape,
-        measuretexture,
-    )
-
-    return {
-        "radial_distribution": measureobjectintensitydistribution.get_radial_distribution,
-        "radial_zernikes": measureobjectintensitydistribution.get_radial_zernikes,
-        "intensity": measureobjectintensity.get_intensity,
-        "sizeshape": measureobjectsizeshape.get_sizeshape,
-        "zernike": measureobjectsizeshape.get_zernike,
-        "ferret": measureobjectsizeshape.get_ferret,
-        "granularity": measuregranularity.get_granularity,
-        "texture": measuretexture.get_texture,
-    }
-
 def get_fast_measurements() -> dict[str, Callable]:
     from cp_measure.fast import (
         measuregranularity,
