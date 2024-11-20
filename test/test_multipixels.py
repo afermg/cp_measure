9238258fb7802fee60ec3070e5cd7e91d3015730
Alán F. Muñoz <afer.mg@gmail.com>
2024-11-20 18:30:46 -0500

    tests: fix multimask tests, apply ruff

--- test/test_multipixels.py
+++ test/test_multipixels.py
@@ -1,18 +1,18 @@
 """
 Test functions that operate multiple pixel arrays (e.g., correlations).
 """
+
 import numpy
 import pytest
 
 from cp_measure.bulk import get_correlation_measurements
-from cp_measure.examples import get_pixels, get_masks
+from cp_measure.examples import get_masks, get_pixels
+
 
 @pytest.mark.parametrize("named_mask", get_masks().items())
 @pytest.mark.parametrize("pixels", [get_pixels()])
 def test_measurements(named_mask: tuple[str, numpy.ndarray], pixels: numpy.ndarray):
-    exceptions = (("one", "costes"),
-                  ("one", "pearson"),
-                  ("two", "costes"))
+    exceptions = (("one", "costes"), ("one", "pearson"), ("two", "costes"))
     mask_name, mask = named_mask
     for name, v in get_correlation_measurements().items():
         if (mask_name, name) not in exceptions:
@@ -20,8 +20,8 @@ def test_measurements(named_mask: tuple[str, numpy.ndarray], pixels: numpy.ndarr
             text = f"Feature {name} returned zero/null on mask {mask_name}"
 
             # Test that at least one item contains a valid value
-            assert all(
-                        [any(~(x == 0 | numpy.isnan(x))) for x in result.values()]
-                    ), text
+            assert all([any(~(x == 0 | numpy.isnan(x))) for x in result.values()]), text
             # Test that the output and number of masks match
-            assert all([len(x)==mask.max() for x in result.values()]), f"Input-Output size does not match: Feature {name}, mask {mask_name}"
+            assert all(
+                [len(x) == mask.max() for x in result.values()]
+            ), f"Input-Output size does not match: Feature {name}, mask {mask_name}"
