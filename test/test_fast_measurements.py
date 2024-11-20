6886ed28efd3bfce9d7f8ea0ecbec88bfcb9db04
Alán F. Muñoz <afer.mg@gmail.com>
2024-11-20 18:31:36 -0500

    test: apply ruff formatting

--- test/test_fast_measurements.py
+++ test/test_fast_measurements.py
@@ -2,9 +2,9 @@ from itertools import product
 
 import numpy
 import pytest
-
 from cp_measure.bulk import get_fast_measurements
-from cp_measure.examples import get_pixels, get_masks
+from cp_measure.examples import get_masks, get_pixels
+
 
 @pytest.mark.parametrize("named_mask", get_masks().items())
 @pytest.mark.parametrize("pixels", [get_pixels()])
@@ -20,9 +20,7 @@ def test_measurements(named_mask: tuple[str, numpy.ndarray], pixels: numpy.ndarr
         ),
     )
     mask_name, mask = named_mask
-    for i, (name, v) in enumerate(get_fast_measurements().items()):
-        # if name=="texture" and mask_name=="center":
-        #     breakpoint()
+    for name, v in get_fast_measurements().items():
         result = v(mask, pixels.copy())
         if (mask_name, name) not in exceptions:
             text = f"Feature {name} returned zero/null on mask {mask_name}"
@@ -32,6 +30,8 @@ def test_measurements(named_mask: tuple[str, numpy.ndarray], pixels: numpy.ndarr
                     [any(~(x == 0 | numpy.isnan(x))) for x in result.values()]
                 ), text
                 # Test that the output and number of masks match
-                assert all([len(x)==mask.max() for x in result.values()]), f"Input-Output size does not match: Feature {name}, mask {mask_name}"
+                assert all(
+                    [len(x) == mask.max() for x in result.values()]
+                ), f"Input-Output size does not match: Feature {name}, mask {mask_name}"
             else:
                 assert result != 0 and not numpy.isnan(result), text
