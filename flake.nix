e44f8686dce62f42bca84e682b12e6c4267cbeb2
Alán F. Muñoz <afer.mg@gmail.com>
2026-01-14 16:11:09 -0700

    feat: support py313 and py314
    * feat: support py313 by using a relaxed scikit-image from centrosome
    
    * deps: relax constraints
    
    * test: remove py39, add py313
    
    * deps: cleanup and fix typo
    
    * tweak: remove editable centrosome
    
    * chore(deps): Upgrade scikit-image and centrosome
    
    Update scikit-image to >=0.25 and centrosome to >=1.3.3 in
    pyproject.toml and refresh the lockfile.
    
    * feat: Add support for Python 3.14
    
    Update the CI workflow test matrix and the project metadata in
    pyproject.toml to include support for Python 3.14.

--- flake.nix
+++ flake.nix
@@ -7,67 +7,81 @@
     flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs, flake-utils, systems, ... } @ inputs:
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs = import nixpkgs {
-              system = system;
-              config.allowUnfree = true;
-            };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      systems,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
 
-            mpkgs = import inputs.nixpkgs_master {
-              system = system;
-              config.allowUnfree = true;
-            };
+        mpkgs = import inputs.nixpkgs_master {
+          system = system;
+          config.allowUnfree = true;
+        };
 
-            libList = [
-                # Add needed packages here
-                pkgs.libz # Numpy
-                pkgs.stdenv.cc.cc
-                pkgs.libGL
-                pkgs.glib
-              ];
-          in
-          with pkgs;
-        {
-          devShells = {
-            default  = let
+        libList = [
+          # Add needed packages here
+          pkgs.libz # Numpy
+          pkgs.stdenv.cc.cc
+          pkgs.libGL
+          pkgs.glib
+        ];
+      in
+      with pkgs;
+      {
+        devShells = {
+          default =
+            let
               # These packages get built by Nix, and will be ahead on the PATH
-                pwp = (python312.withPackages (p: with p; [
-                     python-lsp-server
-                     python-lsp-ruff
-                     venvShellHook
-                   ]));
-            in mkShell {
-               NIX_LD = runCommand "ld.so" {} ''
-                        ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
-                      '';
-                NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
-                packages = [
-                  pkgs.gcc
-                  pwp
-                  uv
-                ]
-                ++ libList;
-                venvDir = "./.venv";
-                postVenvCreation = ''
-                    unset SOURCE_DATE_EPOCH
-                  '';
-                postShellHook = ''
-                    unset SOURCE_DATE_EPOCH
-                  '';
-                shellHook = ''
-                    export UV_PYTHON=${pkgs.python312}
-                    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
-                    export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+              pwp = (
+                python313.withPackages (
+                  p: with p; [
+                    python-lsp-server
+                    python-lsp-ruff
+                    venvShellHook
+                  ]
+                )
+              );
+            in
+            mkShell {
+              NIX_LD = runCommand "ld.so" { } ''
+                ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+              '';
+              NIX_LD_LIBRARY_PATH = lib.makeLibraryPath libList;
+              packages = [
+                pwp
+                uv
+                pkgs.gcc
+              ]
+              ++ libList;
+              venvDir = "./.venv";
+              postVenvCreation = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              postShellHook = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              shellHook = ''
+                export UV_PYTHON=${pkgs.python313}
+                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+                export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
 
-                    uv sync --all-extras
-                    export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
-                    runHook venvShellHook
-                    source .venv/bin/activate
-                '';
-             };
-          };
-        }
-      );
+                uv sync --all-groups
+                export PYTHONPATH=${pwp}/${pwp.sitePackages}:$PYTHONPATH
+                runHook venvShellHook
+                source .venv/bin/activate
+              '';
+            };
+        };
+      }
+    );
 }
